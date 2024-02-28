63820bed11a2d1eacc8deac777792b27c58b84ba
newtoallofthis123 <noobscience123@gmail.com>
2024-02-28 22:42:18 +0530

    Working Prototype

--- content.php
+++ content.php
@@ -22,10 +22,16 @@ $main[] = $controls = new HTML(tag: 'div', classes: ['flex ', 'flex-col ', 'just
 //     content: 'Start'
 // );
 $controls[] = new HTML(tag: 'button', classes: ['border-0 ', 'rounded-md ', 'bg-lime-400 ', 'p-2 ', 'mt-3 ', 'w-2/5 '], content: 'Start', attributes: ['id' => 'start', 'data-action' => 'start.questionaire']);
-$main[] = $content = new HTML(tag: 'div', attributes: ['id' => 'content'], classes: ['flex ', 'flex-col', 'justify-center ', 'items-center ', 'gap-3 ', 'mt-3']);
-$content[] = new HTML(tag: 'p', attributes: ['id' => 'question_text'], classes: ['text-center ', 'pt-3 ', 'text-lg ', 'w-1/5'], content: 'Click start to begin the questionaire.');
+$main[] = new HTML(tag: 'p', attributes: ['id' => 'question_text'], classes: ['text-center ', 'pt-3 ', 'text-lg '], content: 'Click start to begin the questionaire.');
+$main[] = $content = new HTML(tag: 'div', attributes: ['id' => 'content'], classes: ['flex ', 'flex-row', 'justify-center ', 'items-center ', 'gap-3 ', 'mt-3']);
 
-$select = new HTML(tag: 'select', classes: ['border-2 ', 'border-gray-300 ', 'rounded-md ', 'p-2 ', 'w-3/5 ', 'mt-3 '], attributes: ['name' => 'question', 'id' => 'question']);
+$select = new HTML(tag: 'select', classes: ['border-2 ', 'border-gray-300 ', 'rounded-md ', 'w-full ', 'p-2 ', 'mt-3 '], attributes: ['name' => 'question', 'id' => 'question']);
+$select[] = new HTML(tag: 'option', attributes: ['value' => 'Internship', 'data-activate' => "duration;coolness"], content: 'Internship');
+$select[] = new HTML(tag: 'option', attributes: ['value' => 'Job', 'data-activate' => "salary;coolness"], content: 'Job');
 
-$content[] = $select;
-$content[] = new HTML(tag: 'button', classes: ['border-0 ', 'rounded-md ', 'bg-red-400 ', 'p-2 ', 'mt-3 ', 'w-1/5 '], content: 'Submit', attributes: ['id' => 'start']);
+$select_div = new HTML(tag: 'div', classes: ['hidden ', 'w-full'], attributes: ['id' => 'questions']);
+$select_div[] = $select;
+$select_div[] = new HTML(tag: 'div');
+
+$content[] = $select_div;
+$main[] = new HTML(tag: 'button', classes: ['border-0 ', 'rounded-md ', 'bg-red-400 ', 'p-2 ', 'mt-3 ', 'w-1/5 '], content: 'Submit', attributes: ['id' => 'start']);
--- static/js/noobscience/questionaire.js
+++ static/js/noobscience/questionaire.js
@@ -8,6 +8,10 @@ NoobScience.Questionaire.main = function(config = {}) {
         select: {
             selector: '#question',
         },
+        selectParent: {
+            selector: '#questions',
+            children: '#questions>div'
+        },
         start: {
             selector: '#start',
         }
@@ -15,18 +19,92 @@ NoobScience.Questionaire.main = function(config = {}) {
     overwriteDefaults(config, $elf.config);
 
     $elf.init = function() {
-        $(config.select.selector).on('change.questionaire', $elf.dispatch.onChange);
-        $(config.start.selector).on('click.start', $elf.dispatch.onStart);
+        $($elf.config.select.selector).each(function() {
+            $(this).on('change', dispatch.onChange);
+        })
+        $($elf.config.selectParent.children).each(function() {
+            $(this).on('change', dispatch.onChange);
+        })
+        $($elf.config.start.selector).on('click.start', dispatch.onStart);
     };
 
-    $elf.dispatch = {
+    // since we are not yet sure of the options, we will hard code them
+    let options = {
+        'coolness': {
+            "options": [{
+                'I am very cool': ['duration', 'salary']
+            },
+            { 'I am not cool': ['duration', 'salary'] },
+            { 'I am not sure': ['duration', 'salary'] }
+            ]
+        },
+        'duration': {
+            "options": [{
+                '1 year': ['salary']
+            },
+            { '2 years': ['salary'] },
+            { '3 years': ['salary'] }
+            ]
+        },
+        'salary': {
+            "options": [{
+                '100,000': []
+            },
+            { '200,000': [] },
+            { '300,000': [] }
+            ]
+        }
+    }
+
+    let dispatch = {
         onChange: function() {
-            console.log("Change");
+            // get the selector of the option selected
+            let selected = $(this).val();
+            // get the data attributes of the selected option
+            let dataAttributes = $(this).find('option:selected').data('activate').split(';');
+
+            $($elf.config.selectParent.children).empty();
+            dataAttributes.map(function(attribute) {
+                $($elf.config.selectParent.children).append(utilities.makeSelect(options[attribute]));
+            });
         },
         onStart: function() {
-            console.log("Start");
+            let select = $elf.config.selectParent.selector;
+            // set display to block
+            $(select).removeClass('hidden');
         }
     };
+
+    let utilities = {
+        makeOption: function(text, activates = []) {
+            let ele = $('<option>').val(text).text(text)
+            // add data-activate attribute to the option
+            console.log(activates);
+            if (activates.length > 0) {
+                ele.attr('data-activate', activates.join(';'));
+            }
+            return ele;
+        },
+        makeSelect: function(options) {
+            let select = $('<select>')
+            let classes = ['border-2 ', 'border-gray-300 ', 'rounded-md ', 'w-full ', 'p-2 ', 'mt-3 '];
+            classes.map(function(className) {
+                select.addClass(className);
+            });
+            // options["options"].map(function(option) {
+            //     console.log(Object.keys(option));
+            //     select.append(utilities.makeOption(Object.keys(option)[0], option[Object.keys(option)[0]]));
+            // });
+            for (let i = 0; i < options["options"].length; i++) {
+                let option = options["options"][i];
+                select.append(utilities.makeOption(Object.keys(option)[0], option[Object.keys(option)[0]]));
+            }
+            return select;
+        }
+    }
+
+    $elf.init();
+    return $elf;
 };
 
-export let Questionaire = new NoobScience.Questionaire.main;
+export let Questionaire = NoobScience.Questionaire.main;
